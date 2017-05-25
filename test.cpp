3199060c2dd96062035687f566c596ea65945dc7
Andreas Röver <roever@users.sf.net>
2017-05-25 16:35:22 +0200

    fix warnings

--- test.cpp
+++ test.cpp
@@ -7,8 +7,8 @@ int main()
   char * img = new char [width*height*3];
   const char * data = header_data;
 
-  for (int y = 0; y < height; y++)
-    for (int x = 0; x < width; x++)
+  for (unsigned int y = 0; y < height; y++)
+    for (unsigned int x = 0; x < width; x++)
       HEADER_PIXEL(data, (&img[(y*width+x)*3]));
 
   MSG_ShowSimpleImageBox("Title äöß", width, height, img);
